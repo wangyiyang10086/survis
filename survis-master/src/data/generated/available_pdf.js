fd127adf43a642dd5b2762380549f93e019b7926
wangyiyang10086 <67257085+wangyiyang10086@users.noreply.github.com>
2023-04-07 10:15:21 +0100

    Add files via upload

---
+++ survis-master/src/data/generated/available_pdf.js
@@ -0,0 +1,1 @@
+const availablePdf = ["BibTeX 1","BibTeX 10","BibTeX 2","BibTeX 3","BibTeX 4","BibTeX 5","BibTeX 6","BibTeX 7","BibTeX 8","BibTeX 9"];
---
+++ survis-master/src/data/generated/bib.js
@@ -0,0 +1,112 @@
+﻿const generatedBibEntries = {
+    "Aminifar, Farrokh 2021": {
+        "abstract": "Power system protection and asset management have drawn the attention of researchers for several decades; but they still suffer from unresolved and challenging technical issues. The situation has been recently exacerbated in the wake of the ever-changing landscape of power systems driven by the growing uncertainty and volatility subsequent to the vast renewable energy integration, more frequent natural extreme events due to climate changes, increasing malicious cyberattacks, and more constrained transmission systems as the result of load growth and limited investments. On the opposite side, the proliferation of advanced measuring devices such as phasor measurement units, emerging electric and non-electric sensors, and Internet of Thing (IoT)-enabled data gathering platforms continually expand/nourish the databases; they hence offer unprecedented opportunities to take the advantage of data-driven techniques. Machine learning (ML) as a principal class of artificial intelligence is the perfect match solution to this need and has newly revoked many researchers\u2019 interests to tackle the problems excluding their exact/detailed models. This paper aims to provide an overview on applications of ML techniques in power system protection and asset management. This paper elaborates on issues pertaining to (1) synchronous generators, (2) power transformers, (3) transmission lines, and (4) special and system-integrity protection schemes. In addition to the opportunities offered by the ML techniques, this paper discourses on the barriers and challenges to the wide-spread application of ML techniques in real-world practices.",
+        "author": "Aminifar, Farrokh ; Abedini, Moein ; Amraee, Turaj ; Jafarian, Peyman ; Samimi, Mohammad Hamed ; Shahidehpour, Mohammad",
+        "doi": "https://doi.org/10.1007/s12667-021-00448-6",
+        "keywords": "type:Comparison of machine learning models,Data processing models",
+        "number": "06",
+        "title": "A review of power system protection and asset management with machine learning techniques",
+        "type": "article",
+        "volume": "22",
+        "year": "2021"
+    },
+    "Chen, ZhiYuan2013": {
+        "abstract": "The research and development of Mission Critical System such as national communication system, national grid system and oil and gas pipeline network has been a great concern. Many innovative technologies have provided solutions and applications to the stability and continuous system operation. However a minute of interruption may cost millions of dollars and there were a lot of incident cases, which have brought bad impact to the economy. Therefore the purpose of this project is to design and implement a real time fault prediction system based on the support vector machine in order to meet the robustness requirements of a mission critical system. Current systems seldom implement the artificial intelligence such as Support Vector Machine due to the high cost of computational and no proven real-time artificial intelligence methods suitable for real-time systems. In this project we propose an intelligent system with an unsupervised real time prediction engine to detect faults. Meanwhile the issue of reducing the human intervention in the training phase has also been addressed by using an efficient strategic approach, which is a Zero Human Intervention Operation system that combines the support vector machine, clustering, and automatic parameter learning algorithms.",
+        "author": "Chen, Zhiyuan; Isa Dino; Nik Ahmad Akram",
+        "doi": "http://arxiv.org/abs/2012.15032",
+        "keywords": "type:Comparison of machine learning models,Data processing models",
+        "number": "02",
+        "title": "Unsupervised Real Time Prediction of Faults Using the Support Vector Machine",
+        "type": "article",
+        "volume": "22",
+        "year": "2013"
+    },
+    "Chen, ZhiYuan2016": {
+        "abstract": "This paper aims at improving the classification accuracy of a Support Vector Machine (SVM) classifier with Sequential Minimal Optimization (SMO) training algorithm in order to properly classify failure and normal instances from oil and gas equipment data. Recent applications of failure analysis have made use of the SVM technique without implementing SMO training algorithm, while in our study we show that the proposed solution can perform much better when using the SMO training algorithm. Furthermore, we implement the ensemble approach, which is a hybrid rule based and neural network classifier to improve the performance of the SVM classifier (with SMO training algorithm). The optimization study is as a result of the underperformance of the classifier when dealing with imbalanced dataset. The selected best performing classifiers are combined together with SVM classifier (with SMO training algorithm) by using the stacking ensemble method which is to create an efficient ensemble predictive model that can handle the issue of imbalanced data. The classification performance of this predictive model is considerably better than the SVM with and without SMO training algorithm and many other conventional classifiers.",
+        "author": "Chen, ZhiYuan; Selere, Olugbenro O; Nicholas Lu Chee Seng",
+        "doi": "https://doi.org/10.48550/arXiv.2012.15030",
+        "keywords": "type:Comparison of machine learning models,Data processing models",
+        "number": "01",
+        "title": "Equipment Failure Analysis for Oil and Gas Industry with an Ensemble Predictive Model",
+        "type": "article",
+        "volume": "22",
+        "year": "2016"
+    },
+    "Heaton, J. B. 2016": {
+        "abstract": "We explore the use of deep learning hierarchical models for problems in financial prediction and classification. Financial prediction problems \u2013 such as those presented in designing and pricing securities, constructing portfolios, and risk management \u2013 often involve large data sets with complex data interactions that currently are difficult or impossible to specify in a full economic model. Applying deep learning methods to these problems can produce more useful results than standard methods in finance. In particular, deep learning can detect and exploit interactions in the data that are, at least currently, invisible to any existing financial economic theory.",
+        "author": "Heaton, J. B. ; Polson, N. G. ; Witte, J. H.",
+        "doi": "https://doi-org.nottingham.idm.oclc.org/10.1002/asmb.2209",
+        "keywords": "type:Comparison of machine learning models,Data processing models",
+        "number": "03",
+        "title": "Deep learning for finance: deep portfolios",
+        "type": "article",
+        "volume": "22",
+        "year": "2016"
+    },
+    "Liangyu Min 2021": {
+        "abstract": "Conservatism is the notorious problem of the worst-case robust portfolio optimization, and this issue has raised broad discussion in academia. To this end, we propose the hybrid robust portfolio models under ellipsoidal uncertainty sets in this paper, where both the best-case and the worst-case counterparts are involved. In the suggested models, we introduce a trade-off parameter to adjust the portfolio optimism level. Machine learning algorithms including Long Short-Term Memory (LSTM) and eXtreme Gradient Boosting (XGBoost) are used to evaluate the potential market movements and provide forecasting information to generate the hyperparameter for modeling. Additionally, we develop a clustering-based algorithm for properly constructing joint ellipsoidal uncertainty sets to reduce conservatism further. In the modeling phase, we design the hybrid portfolios based on variance (HRMV) and value at risk (VaR) and prove the equivalent relationship between the hybrid robust mean-VaR model (HRMVaR) and the hybrid robust mean-CVaR (conditional value at risk) according to the existing research. The US 12 industry portfolio data set retrieved from Kenneth R. French is employed for the in-sample and out-of-sample numerical experiments. The experimental results demonstrate the effectiveness and robustness of the proposed portfolios, where HRMV models have better Sharpe ratios and Calmar ratios than the corresponding nominal mean\u2013variance model, and HRMVaR models outperform the baseline VaR-based portfolios in terms of returns. Sensitivity analysis supports the superiority of the joint ellipsoidal uncertainty set , where the proposed portfolios constrained with  show stable risk characteristics.",
+        "author": "Liangyu Min, Jiawei Dong, Jiangwei Liu, Xiaomin Gong",
+        "doi": "https://doi.org/10.1016/j.asoc.2021.107948",
+        "keywords": "type:Asset regulation in the fintech industry,type:Comparison of machine learning models",
+        "number": "08",
+        "title": "Robust mean-risk portfolio optimization using machine learning-based trade-off parameter",
+        "type": "article",
+        "volume": "22",
+        "year": "2021"
+    },
+    "Noella Nazareth 2023": {
+        "abstract": "This systematic literature review analyses the recent advances of machine learning and deep learning in finance. The study considers six financial domains: stock markets, portfolio management, cryptocurrency, forex markets, financial crisis, bankruptcy and insolvency. We provide an overview of previously proposed techniques in these areas by examining 126 selected articles across 44 reputed journals. The main contributions of this review include an extensive examination of data characteristics and features used for model training, evaluation of validation approaches, and model performance addressing each financial problem. A systematic literature review methodology, PRISMA, is used to carry out this comprehensive review. The study also analyses bibliometric information to understand the current status of research focused on machine learning in finance. The study finally points out possible research directions which might lead to new inquiries in machine learning and finance.",
+        "author": "Noella Nazareth , Yeruva Venkata Ramana Reddy",
+        "doi": "https://doi.org/10.1016/j.eswa.2023.119640",
+        "keywords": "type:Asset regulation in the fintech industry",
+        "number": "05",
+        "title": "Financial applications of machine learning A literature review",
+        "type": "article",
+        "volume": "22",
+        "year": "2023"
+    },
+    "Rahbaralam, Maryam  2007": {
+        "abstract": "Machine learning components are essential for today\u2019s software systems, causing a need to adapt traditional software engineering practices when developing machine-learning-based systems. This need is pronounced due to many development-related challenges of machine learning components such as asset, experiment, and dependency management. Recently, many asset management tools addressing these challenges have become available. It is essential to understand the support such tools offer to facilitate research and practice on building new management tools with native supports for machine learning and software engineering assets. This article positions machine learning asset management as a discipline that provides improved methods and tools for performing operations on machine learning assets. We present a feature-based survey of 18 state-of-practice and 12 state-of-research tools supporting machine-learning asset management. We overview their features for managing the types of assets used in machine learning experiments. Most state-of-research tools focus on tracking, exploring, and retrieving assets to address development concerns such as reproducibility, while the state-of-practice tools also offer collaboration and workflow-execution-related operations. In addition, assets are primarily tracked intrusively from the source code through APIs and managed via web dashboards or command-line interfaces (CLIs). We identify asynchronous collaboration and asset reusability as directions for new tools and techniques.",
+        "author": "Rahbaralam, Maryam; Modesto, David; Card\u00fas, Jaume; Abdollahi, Amir; Cucchietti, Fernando M.",
+        "doi": "http://arxiv.org/abs/2007.03744",
+        "keywords": "type:Comparison of machine learning models,Data processing models",
+        "number": "10",
+        "title": "Predictive Analytics for Water Asset Management Machine Learning and Survival Analysis",
+        "type": "article",
+        "volume": "22",
+        "year": "2007"
+    },
+    "Samuel Idowu  2022": {
+        "abstract": "Machine learning components are essential for today\u2019s software systems, causing a need to adapt traditional software engineering practices when developing machine-learning-based systems. This need is pronounced due to many development-related challenges of machine learning components such as asset, experiment, and dependency management. Recently, many asset management tools addressing these challenges have become available. It is essential to understand the support such tools offer to facilitate research and practice on building new management tools with native supports for machine learning and software engineering assets. This article positions machine learning asset management as a discipline that provides improved methods and tools for performing operations on machine learning assets. We present a feature-based survey of 18 state-of-practice and 12 state-of-research tools supporting machine-learning asset management. We overview their features for managing the types of assets used in machine learning experiments. Most state-of-research tools focus on tracking, exploring, and retrieving assets to address development concerns such as reproducibility, while the state-of-practice tools also offer collaboration and workflow-execution-related operations. In addition, assets are primarily tracked intrusively from the source code through APIs and managed via web dashboards or command-line interfaces (CLIs). We identify asynchronous collaboration and asset reusability as directions for new tools and techniques.",
+        "author": "Samuel Idowu ,Daniel Str\u00fcber,Thorsten Berger",
+        "doi": "https://doi.org/10.1145/3543847",
+        "keywords": "type:Comparison of machine learning models",
+        "number": "09",
+        "title": "Asset Management in Machine Learning State-of-research and State-of-practice",
+        "type": "article",
+        "volume": "22",
+        "year": "2022"
+    },
+    "Saqib Aziz 2021": {
+        "abstract": "We identify the core topics of research applying machine learning to finance. We use a probabilistic topic modeling approach to make sense of this diverse body of research spanning across multiple disciplines. Through a latent Dirichlet allocation topic modeling technique, we extract 15 coherent research topics that are the focus of 5942 academic studies from 1990 to 2020. We find that these topics can be grouped into four categories: Price-forecasting techniques, financial markets analysis, risk forecasting and financial perspectives. We first describe and structure these topics and then further show how the topic focus has evolved over the last three decades. A notable trend we find is the emergence of text-based machine learning, for example, for sentiment analysis, in recent years. Our study thus provides a structured topography for finance researchers seeking to integrate machine learning research approaches in their exploration of finance phenomena. We also showcase the benefits to finance researchers of the method of probabilistic modeling of topics for deep comprehension of a body of literature.",
+        "author": "Saqib Aziz, Michael Dowling, Helmi Hammami, Anke Piepenbrink",
+        "doi": "https://doi-org.nottingham.idm.oclc.org/10.1111/eufm.12326",
+        "keywords": "type:Asset regulation in the fintech industry",
+        "number": "04",
+        "title": "Machine learning in finance: A topic modeling approach",
+        "type": "article",
+        "volume": "22",
+        "year": "2021"
+    },
+    "Sim\u00e3o Moraes Sarmento 2020": {
+        "abstract": "Pairs Trading is one of the most valuable market-neutral strategies used by hedge funds. It is particularly interesting because it overcomes the arduous process of valuing securities by focusing on relative pricing. By buying a relatively undervalued security and selling a relatively overvalued one, a profit can be made upon the pair\u2019s price convergence. However, with the growing availability of data, it became increasingly harder to find rewarding pairs. In this work, we address two problems: (i) how to find profitable pairs while constraining the search space and (ii) how to avoid long decline periods due to prolonged divergent pairs. To manage these difficulties, the application of promising Machine Learning techniques is investigated in detail. We propose the integration of an Unsupervised Learning algorithm, OPTICS, to handle problem (i). The results obtained demonstrate the suggested technique can outperform the common pairs\u2019 search methods, achieving an average portfolio Sharpe ratio of 3.79, in comparison to 3.58 and 2.59 obtained by standard approaches. For problem (ii), we introduce a forecasting-based trading model, capable of reducing the periods of portfolio decline by 75%. Yet, this comes at the expense of decreasing overall profitability. The proposed strategy is tested using an ARMA model, an LSTM and an LSTM Encoder-Decoder. This work\u2019s results are simulated during varying periods between January 2009 and December 2018, using 5-min price data from a group of 208 commodity-linked ETFs, and accounting for transaction costs.",
+        "author": "Sim\u00e3o Moraes Sarmento, Nuno Horta",
+        "doi": "https://doi.org/10.1016/j.eswa.2020.113490",
+        "keywords": "type:Asset regulation in the fintech industry",
+        "number": "07",
+        "title": "Enhancing a Pairs Trading strategy with the application of Machine Learning",
+        "type": "article",
+        "volume": "22",
+        "year": "2020"
+    }
+};
